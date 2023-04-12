42ea00cf1087b7c85031419055919ddee3a80584
githubjazon <jason.sevenants@gmail.com>
2023-04-12 10:50:48 +0200

    12avril10h50

---
+++ exoSqlDevAppli/filRougeSql/requetesSqlFilRouge.sql
@@ -0,0 +1,27 @@
+--Ecrire des requêtes d'interrogation de la base de données
+
+
+
+    --Afficher la liste des commandes ( la liste doit faire apparaitre la date, les informations du client, le plat et le prix )
+
+        
+
+    Afficher la liste des plats en spécifiant la catégorie
+
+    Afficher les catégories et le nombre de plats actifs dans chaque catégorie
+
+    Liste des plats les plus vendus par ordre décroissant
+
+    Le plat le plus rémunérateur
+
+    Liste des clients et le chiffre d'affaire généré par client (par ordre décroissant)
+
+--Ecrire des requêtes de modification de la base de données
+
+    Ecrivez une requête permettant de supprimer les plats non actif de la base de données
+
+    Ecrivez une requête permettant de supprimer les commandes avec le statut livré
+
+    Ecrivez un script sql permettant d'ajouter une nouvelle catégorie et un plat dans cette nouvelle catégorie.
+
+    Ecrivez une requête permettant d'augmenter de 10% le prix des plats de la catégorie 'Pizza'
