9fc0744761ba392e37808ef27874fc00321b22ac
githubjazon <jason.sevenants@gmail.com>
2023-04-19 15:21:59 +0200

    FiniEvalSQL_19avril15h21

--- exoSqlDevAppli/filRougeSql/requetesSqlFilRouge.sql
+++ exoSqlDevAppli/filRougeSql/requetesSqlFilRouge.sql
@@ -14,7 +14,7 @@
         p.prix AS"Prix" 
       FROM commande c
       JOIN plat p
-      ON c.id_plat = p.id_categorie
+      ON c.id_plat = p.id
       ORDER BY c.date_commande;
 
 
@@ -29,22 +29,61 @@
       ORDER BY p.id_categorie ASC;
 
 
-    -- Afficher les catégories et le nombre de plats actifs dans chaque catégorie
+    -- Afficher les catégories et le nombre de plats actifs dans chaque catégorie 
 
-      
+      SELECT c.libelle AS categorie, COUNT(p.id) AS nombre_plats_actifs
+      FROM categorie c
+      LEFT JOIN plat p ON c.id = p.id_categorie
+      WHERE p.active = 'actif'
+      GROUP BY c.libelle; --Résultat vide ?
 
-    Liste des plats les plus vendus par ordre décroissant
 
-    Le plat le plus rémunérateur
+    -- Liste des plats les plus vendus par ordre décroissant
 
-    Liste des clients et le chiffre d'affaire généré par client (par ordre décroissant)
+      SELECT p.id, p.libelle AS"Nom du plat", COUNT(c.id_plat) AS "nombre de commandes"
+      FROM plat p
+      LEFT JOIN commande c ON p.id = c.id_plat
+      GROUP BY p.id, p.libelle
+      ORDER BY c.id_plat DESC;
+
+
+    -- Le plat le plus rémunérateur
+
+      SELECT p.id, p.libelle AS"Nom du plat", p.prix, SUM(c.total) AS "Revenu total"
+      FROM plat p
+      LEFT JOIN commande c ON p.id = c.id_plat
+      GROUP BY p.id, p.libelle, p.prix
+      ORDER BY c.total DESC
+      LIMIT 1;
+
+    -- Liste des clients et le chiffre d'affaire généré par client (par ordre décroissant)
+
+
 
 --Ecrire des requêtes de modification de la base de données
 
-    Ecrivez une requête permettant de supprimer les plats non actif de la base de données
+    -- Ecrivez une requête permettant de supprimer les plats non actif de la base de données
 
-    Ecrivez une requête permettant de supprimer les commandes avec le statut livré
+      DELETE FROM plat
+      WHERE active = 'non';
 
-    Ecrivez un script sql permettant d'ajouter une nouvelle catégorie et un plat dans cette nouvelle catégorie.
+    -- Ecrivez une requête permettant de supprimer les commandes avec le statut livré
 
-    Ecrivez une requête permettant d'augmenter de 10% le prix des plats de la catégorie 'Pizza'
+      DELETE FROM commande
+      WHERE etat = 'livré';
+
+    -- Ecrivez un script sql permettant d'ajouter une nouvelle catégorie et un plat dans cette nouvelle catégorie.
+
+      -- Ajouter une nouvelle catégorie
+      INSERT INTO categorie (libelle, image, active)
+      VALUES ('Nouvelle catégorie', 'image.jpg', 'oui');
+      SET @id_categorie = LAST_INSERT_ID();
+      INSERT INTO plat (libelle, description, prix, image, id_categorie, active)
+      VALUES ('Nouveau plat', 'Description du nouveau plat', 9.99, 'plat.jpg', @id_categorie, 'oui');
+
+
+    -- Ecrivez une requête permettant d'augmenter de 10% le prix des plats de la catégorie 'Pizza'
+
+      UPDATE plat
+      SET prix = prix * 1.1
+      WHERE id_categorie = (SELECT id_categorie FROM categorie WHERE libelle = 'Pizza');
